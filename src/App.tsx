b24f47664c9ff87b0dff8bf3f6a581540ad112a9
garry <tahinjanaharygarry@gmail.com>
2025-04-28 09:56:26 +1400

    feat: add Testimonials section with styling and integrate into App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Questions from "./sections/Questions";
 import Services from "./sections/Services";
+import Testimonials from "./sections/Testimonials";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Questions />
       <Services />
       <AboutUs />
+      <Testimonials />
     </>
   );
 }
---
+++ src/sections/Testimonials.tsx
@@ -0,0 +1,15 @@
+const Testimonials = () => {
+  return (
+    <section className="wrapper bg-blue-gray-900">
+      <div className="pt-20">
+        <h1 className="h1_large grid grid-cols-1 ">
+          <span className="testimonial_h1">client</span>
+          <span className="text-white">success</span>
+          <span className="testimonial_h1">stories</span>
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
